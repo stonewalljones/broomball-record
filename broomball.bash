01c22ab56286d39c33920abce9c43245a061572f
stonewall <zrjones12@gmail.com>
2018-01-22 10:57:12 -0500

    Add argument handling

--- broomball.bash
+++ broomball.bash
@@ -4,3 +4,43 @@
 #IFS=$'\n\t'
 
 
+
+usage()
+{
+echo "placeholder"
+}
+
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -d | --directory )      shift
+                                directory=$1
+				echo "$directory"
+                                ;;
+        -t | --time )           shift
+				gametime=$1
+				echo "$gametime"
+                                ;;
+	-T | --team )		shift
+				teamname=$1
+				;;
+	-B | --before )		shift
+				before=$1
+				;;
+	-A | --after )		shift
+				after=$1
+				;;
+	-b | --black )		rink=black
+				;;
+	-g | --gold )		rink=gold
+				;;
+	-s | --silver )		rink=silver
+				;;
+        -h | --help )           usage
+                                exit
+                                ;;
+        * )                     usage
+                                exit 1
+    esac
+    shift
+done
